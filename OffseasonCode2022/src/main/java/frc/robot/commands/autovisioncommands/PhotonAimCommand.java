709e93004636da4cb1f9c5e827bd93024c6926a6
MattCooke <cooke_matthew@hotmail.com>
2022-11-16 20:05:27 -0600

    Minor cleanup removed unused inports

--- OffseasonCode2022/src/main/java/frc/robot/commands/autovisioncommands/PhotonAimCommand.java
+++ OffseasonCode2022/src/main/java/frc/robot/commands/autovisioncommands/PhotonAimCommand.java
@@ -2,13 +2,9 @@
 
 package frc.robot.commands.autovisioncommands;
 
-import com.revrobotics.ColorSensorV3.ColorSensorMeasurementRate;
-
 import org.photonvision.PhotonCamera;
 import org.photonvision.PhotonUtils;
 import org.photonvision.common.hardware.VisionLEDMode;
-
-import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.util.Units;
