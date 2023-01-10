577261bb04fd0b12d4eda211a7155041b57e18cc
JackLaFreniere <97800485+JackLaFreniere@users.noreply.github.com>
2023-01-10 17:54:49 -0600

    1/10/23 Removed unused imports in RunIndexerCommand and LoadedMotorSubsystem.

--- OffseasonCode2022/src/main/java/frc/robot/commands/RunIndexerCommand.java
+++ OffseasonCode2022/src/main/java/frc/robot/commands/RunIndexerCommand.java
@@ -3,7 +3,6 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.IndexerSubsystem;
 import frc.robot.subsystems.LoadedMotorSubsystem;
-import frc.lib.util.IndexerSensorUtility;
 
 public class RunIndexerCommand extends CommandBase {
 
